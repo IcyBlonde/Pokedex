a3fc38f21e69ce413408ed332b771e656aa5f55c
IcyBlonde <haynesad4@gmail.com>
2024-07-12 13:25:35 -0400

    Added a party button and adjusted button sizes

--- src/main/dex/Opening.java
+++ src/main/dex/Opening.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.LayoutManager;
 import java.awt.event.*;
@@ -7,12 +8,16 @@ import java.awt.event.*;
 public class Opening {
     private JFrame frame = new JFrame("Welcome");
     private JButton search = new JButton("Search");
+    private JButton team = new JButton("Party");
 
     public Opening(){
+        frame.setLayout(new FlowLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
         frame.add(search);
-        frame.setLayout(new FlowLayout());
+        frame.add(team);
+        search.setPreferredSize(new Dimension(500, 240));;
+        team.setPreferredSize(new Dimension(500, 240));
         frame.setVisible(true);
         frame.getContentPane().add(search);
 
